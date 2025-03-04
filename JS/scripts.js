0eae1a4359809e5cd672bd66942819fcf12bb6dc
Iulcibaev Dmitri <iulcibaevdmitrii@gmail.com>
2025-03-04 23:39:18 +0200

    Add files via upload

--- JS/scripts.js
+++ JS/scripts.js
@@ -91,8 +91,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     entry.target.classList.add("visible");
                 }
             });
-        },
-        { threshold: 0.3 }
+        }, {
+            threshold: 0.3
+        }
     );
 
     sections.forEach((section) => observer.observe(section));
